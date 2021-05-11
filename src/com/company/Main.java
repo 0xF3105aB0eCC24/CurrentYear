b8026aa2a08bcdcf95147d8347c928676a9d34a6
MOGATON <mazurak24@gmail.com>
2021-05-11 21:38:10 +0200

    add solution method

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        System.out.println(solution.getCurrentYear());
+
+    }
+}
---
+++ src/com/company/Solution.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Calendar;
+
+public class Solution {
+
+    private int currentYear;
+
+    public Solution() {
+        this.currentYear = Calendar.getInstance().get(Calendar.YEAR);
+    }
+
+    public int getCurrentYear() {
+        return currentYear;
+    }
+}
